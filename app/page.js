d3bf9746b97641beb374c937b4c8a52f2efb62ef
Fasih Burak <fasihburak@gmail.com>
2025-06-10 09:17:27 +0300

    Edit page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,26 @@
+'use client';
+
+import { useState } from 'react';
 import LikeButton from './like-button';
- 
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
  
-export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
+export default function Match() {
+  const matchId = '9a828ff5-6046-4cf9-bfa9-7f3917d1b8c1';
+  const [firstTeamGoals, setFirstTeamGoals] = useState(null);
+  const [secondTeamGoals, setSecondTeamGoals] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  fetch(`${baseUrl}/api/matches/${matchId}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+
+  
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
